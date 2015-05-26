fbe627a3066e215d2beaab704f90ca2141c63c75
davkim1030 <davkim1030@gmail.com>
2015-05-26 19:30:29 +0900

    tab에 들어 갈 아이콘들(png)추가

--- app/src/main/java/com/yamae/yamaeapp/MainActivity2.java
+++ app/src/main/java/com/yamae/yamaeapp/MainActivity2.java
@@ -13,13 +13,19 @@ public class MainActivity2 extends ActionBarActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
 
         TabHost mTabHost = (TabHost)findViewById(R.id.mainpage_tab);
       //  mTabHost=getTabHost();
 
         mTabHost.setup();
+        TabHost.TabSpec firstTabSpec = mTabHost.newTabSpec("tid1");
+        TabHost.TabSpec secondTabSpec = mTabHost.newTabSpec("tid1");
+        TabHost.TabSpec thirdTabSpec = mTabHost.newTabSpec("tid1");
+
+        firstTabSpec.setIndicator("",getResources().getDrawable(R.drawable.menu_list_icon));
+        secondTabSpec.setIndicator("",getResources().getDrawable(R.drawable.star_icon));
+        thirdTabSpec.setIndicator("",getResources().getDrawable(R.drawable. overflow_icon));
         mTabHost.addTab(mTabHost.newTabSpec("tab_test1")
                         .setContent(R.id.view1).setIndicator("tab_test1")
         );
