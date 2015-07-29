6a237a8ca3ff44075b75aa3848f4e2db934c28b8
songmho <songmho@gmail.com>
2015-07-29 16:01:17 +0900

    fabric 연결

--- app/src/main/java/com/yamae/yamaeapp/YaMaeApp.java
+++ app/src/main/java/com/yamae/yamaeapp/YaMaeApp.java
@@ -2,8 +2,10 @@ package com.yamae.yamaeapp;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.parse.Parse;
 import com.parse.ParseACL;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by HyunWook Kim on 2014-11-02.
@@ -15,6 +17,7 @@ public class YaMaeApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
 
         ParseACL defaultACL=new ParseACL();
